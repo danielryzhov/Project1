f3c6fc28b55e4aeee3e60b4a27cb268bf306d465
Даниэль Рыжов <karamba7771@gmail.com>
2024-07-03 17:46:22 +0300

    'masks.py'

---
+++ src/masks.py
@@ -0,0 +1,15 @@
+def get_mask_card_number(card_number: str) -> str and None:
+    """Функция принимает на вход номер карты и возвращает ее маску"""
+
+    if card_number.isdigit() and len(card_number) == 16:
+        return f"{card_number[0:4]} {card_number[4:6]}** **** {card_number[12:]}"
+    else:
+        return None
+
+
+def get_mask_account(account: str) -> str and None:
+    """Функция принимает на вход номер счета и возвращает его маску"""
+    if account.isdigit() and len(account) == 20:
+        return f"{'*' * 16}{account[16:]}"
+    else:
+        return None
