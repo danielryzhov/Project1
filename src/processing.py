6ae9dbe0fb1b246048101c2d411385aab2a3c94c
Даниэль Рыжов <karamba7771@gmail.com>
2024-07-12 13:16:25 +0300

    processing fixes:docstring, sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -1,14 +1,20 @@
 from typing import Iterable
+from typing import Any
 
-EXECUTED = 'EXECUTED'
 
-def filter_by_state(filter_state: Iterable[str])-> None and Iterable:
+def filter_by_state(filter_state: Iterable, state="EXECUTED") -> None and Iterable:
+    """Функция принимает список словарей и возвращает новый список словарей содержащий только
+    те словари, у которых ключ state соответствует указанному значению"""
     new_filter_state = []
 
     for dictionary_state in filter_state:
-        if dictionary_state['state'] == EXECUTED:
+        if dictionary_state["state"] == state:
             new_filter_state.append(dictionary_state)
 
     return new_filter_state
 
 
+def sort_by_date(sort_state: list[dict[str, Any]], reverse=True) -> list[dict[str, Any]]:
+    """Функция возвращает новый список отсортированный по дате"""
+    sorted_state_date = sorted(sort_state, key=lambda sort_state: sort_state["date"], reverse=reverse)
+    return sorted_state_date
